d9c1590192b7c718748308203162a5df65ab5d61
ZOE <40182161@live.napier.ac.uk>
2017-01-20 22:54:26 +0000

    Enemy added :rooster:

---
+++ Doog/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    { 
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 translation = new Vector3(0.0f, 0.0f, -1.0f);
+
+        translation *= speed * Time.deltaTime;
+
+        this.transform.Translate(translation);
+
+	}
+}
