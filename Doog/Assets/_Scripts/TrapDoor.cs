bf82c2a65499db7fa5b0c1d651b0f18a7712b62f
rasmusmunk <munk1@live.dk>
2017-01-21 20:20:10 +0000

    Trap door in progress

---
+++ Doog/Assets/_Scripts/TrapDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 currentPosition = this.transform.localPosition;
+            this.transform.Translate(new Vector3(0, -0.564f, 0.488f));
+            this.transform.Rotate(new Vector3(70.0f, 0, 0));
+            Debug.Log("Transforming");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+
+
+        }
+    }
+}
