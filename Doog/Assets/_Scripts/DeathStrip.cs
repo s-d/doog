bd7fc67665d92fcd54c2494fae16c3b2d0f80d1e
ZOE <40182161@live.napier.ac.uk>
2017-01-21 03:31:52 +0000

    Spawn Random Die :skull:

---
+++ Doog/Assets/_Scripts/DeathStrip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathStrip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("in here " + other.name);
+
+        if (Input.GetKey("space"))
+            Destroy(other.gameObject);
+    }
+}
--- Doog/Assets/_Scripts/EnemyController.cs
+++ Doog/Assets/_Scripts/EnemyController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using UnityEngine.Windows.Speech;
 using System.Collections;
 
@@ -6,20 +7,19 @@ public class EnemyController : MonoBehaviour {
 
     public float speed;
     private Renderer[] sprites;
-   
 
-
-	void Awake ()
+    void Start()
     {
-        sprites = this.GetComponentsInChildren<Renderer>();
+        PrefabUtility.DisconnectPrefabInstance(this);
 
-       
-        // shoddy code
-        var tex = Resources.Load("Sprites\\Body\\body_01_a") as Texture2D;
-        tex.filterMode = FilterMode.Point;
-        sprites[0].material.shader = Shader.Find("Sprites/Diffuse");
-        sprites[0].material.SetTexture("_MainTex", tex);
-	}
+        // randomly generate 
+        Vector3 initialPos = this.transform.localPosition;
+
+        initialPos.x = Random.Range(-2.5f, 2.5f);
+
+        this.transform.localPosition = initialPos;
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -30,5 +30,25 @@ public class EnemyController : MonoBehaviour {
 
         this.transform.Translate(translation);
 
+        Debug.Log("whee " + this.transform.localPosition.x);
+
+        //TODO
+        // destroy after off screen
+        if (this.transform.localPosition.z <= -14.0f)
+        {
+            Destroy(this);
+        }
+
 	}
+
+    public void SetLook(Texture2D[] textures)
+    {
+        sprites = this.GetComponentsInChildren<Renderer>();
+
+        for (int i = 0; i < textures.Length; ++i)
+        {
+            sprites[i].material.shader = Shader.Find("Sprites/Diffuse");
+            sprites[i].material.SetTexture("_MainTex", textures[i]);
+        }
+    }
 }
