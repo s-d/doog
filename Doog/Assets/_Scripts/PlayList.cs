972dbb04d0a8930f176803eb262f401b3ff899ab
rasmusmunk <munk1@live.dk>
2017-01-22 14:14:08 +0000

    Background tracks now plays when the game scene starts

---
+++ Doog/Assets/_Scripts/PlayList.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayList : MonoBehaviour {
+
+    private int _currentTrack = 0;
+    private AudioSource _audioSource;
+    private AudioClip[] _audioClips;
+    private bool _changing = false;
+
+	// Use this for initialization
+	void Start () {
+        _audioSource = gameObject.GetComponentInChildren<AudioSource>();
+        _audioClips = Resources.LoadAll<AudioClip>("Audio/Drum_Loops");
+        _audioSource.loop = true;
+        _audioSource.clip = _audioClips[_currentTrack];
+        _audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_changing)
+        {
+            Debug.Log("Changing");
+            StartCoroutine(beatChange());
+        }
+	}
+
+    IEnumerator beatChange()
+    {
+        _changing = true;
+        _currentTrack = Random.Range(0, _audioClips.Length);
+        _audioSource.clip = _audioClips[_currentTrack];
+        _audioSource.Play();
+        yield return new WaitForSeconds(15);
+        _changing = false;
+    }
+
+
+    void loadAudioClip(Object[] resources)
+    {
+
+    }
+}
